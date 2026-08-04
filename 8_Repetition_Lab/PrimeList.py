f4d34c47fd49579bcc29c2fdc3583b24dd329395
Sunday Alabi <iamalabisunday@gmail.com>
2026-08-04 15:00:33 +0100

    update

--- 8_Repetition_Lab/PrimeList.py
+++ 8_Repetition_Lab/PrimeList.py
@@ -1,1 +1,13 @@
 # Implement prime_list(n). Return a list of all prime numbers from 2 to n inclusive. A prime number has exactly two positive divisors. Students may need to research a simple primality test or the Sieve of Eratosthenes.
+def prime_list(n):
+    primes = []
+
+    for num in range(2, n + 1):
+        prime = True
+        for i in range(2, num):
+            if num % i == 0:
+                prime = False
+        if prime:
+            primes.append(num)
+
+    return primes
