2b13fe9c200455e955f6cc9fc2a04ce069a90b7c
Sunday Alabi <iamalabisunday@gmail.com>
2026-08-05 09:40:04 +0100

    update

--- 9_List_Wrangler/CleanWords.py
+++ 9_List_Wrangler/CleanWords.py
@@ -1,3 +1,7 @@
 # Implement clean_words(words). Return a new list where each word is stripped of surrounding spaces and converted to lowercase. Ignore words that become empty after stripping. Use a list comprehension.
 def clean_words(words):
-    return [word.strip().lower() for word in words if word.strip()]
+    return [word.strip().lower() for word in words]
+
+result = clean_words([" sunday. "])
+result = clean_words([" sunday. "])
+print(result)
