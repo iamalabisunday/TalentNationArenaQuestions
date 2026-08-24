1411df2bddbd910ecba60d370e5bb7fa052dcf99
Sunday Alabi <iamalabisunday@gmail.com>
2026-08-24 12:53:36 +0100

    Format

--- 0_Greeter_Basics/Format_Multi-line_Output.py
+++ 0_Greeter_Basics/Format_Multi-line_Output.py
@@ -12,9 +12,51 @@
 # - Do not add an extra blank line at the end.
 # - Return the final string.
 
-def solution(name, cohort):
-    return (
-        f"Name: {name}\n"
-        f"Cohort: {cohort}\n"
-        f"Status: Ready"
-    )
+# def solution(name, cohort):
+#     return (
+#         f"Name: {name}\n"
+#         f"Cohort: {cohort}\n"
+#         f"Status: Ready"
+#     )
+
+class Solution:
+    def __init__(self, name: str, cohort: str):
+        self.name = name
+        self.cohort = cohort
+
+    def __str__(self):
+        return (
+            f"Name: {self.name}\n"
+            f"Cohort: {self.cohort}\n"
+            f"Status: Ready"
+        )
+
+    def __repr__(self):
+        return f"Solution: '{self.name}', {self.cohort}"
+
+def ask_to_continue():
+    while True:
+        again = input("Do you want to try again: ").strip().lower()
+        if again in ("y", "yes"):
+            return True
+        elif again in ("n", "now"):
+            return False
+        print("Invalid choice. Please enter 'y' or 'n'.")
+
+def main():
+    print("---------Solution-----------")
+
+    name = input("Enter name: ")
+    cohort = input("Enter cohort: ")
+
+    result = Solution(name, cohort)
+    print("-------------------")
+    print(result)
+    print("-------------------")
+    
+if __name__ == "__main__":
+    while True:
+        main()
+        if not ask_to_continue():
+            print("\nGoodBye")
+            break
