82f77310dabd96929d489c5ca057811dc0378eb9
Sunday Alabi <iamalabisunday@gmail.com>
2026-08-24 13:06:12 +0100

    update

--- 0_Greeter_Basics/Print_Exact_Greeting.py
+++ 0_Greeter_Basics/Print_Exact_Greeting.py
@@ -11,5 +11,37 @@
 # - Do not hardcode only the sample names.
 
 
-def solution(name):
-    return f"Hello, {name}. Welcome to Talent Nation."
+# def solution(name):
+#     return f"Hello, {name}. Welcome to Talent Nation."
+
+# Standard Method
+class Solution:
+    def __init__(self, name: str):
+        self.name = name
+    def __str__(self):
+        return f"Hello, {self.name}. Welcome to Talent Nation"
+    def __repr__(self):
+        return f"Solution {self.name}"
+
+def main():
+    name = input("Enter a name: ")
+    result = Solution(name)
+    print("-------------------")
+    print(result)
+    print("-------------------")
+
+def ask_again():
+    while True:
+        again = input("Do you want to try again: ").lower().strip()
+        if again in ("y", "yes"):
+            return True
+        elif again in ("n", "now"):
+            return False
+        print("Invalid: Kindly enter yes or no")
+
+if __name__ == "__main__":
+    while True:
+        main()
+        if not ask_again():
+            print("Goodbye!")
+            break
